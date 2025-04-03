11dd54e260ceae6cb64309b9383c8a491c1667be
052787 <052787@SPFAM2LAB164-00.famacad.com.br>
2025-04-03 20:43:04 -0300

    Aula 3/4

--- src/main/java/br/com/vemprafam/controller/AlunosController.java
+++ src/main/java/br/com/vemprafam/controller/AlunosController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.vemprafam.dao.DaoAluno;
 import br.com.vemprafam.pojo.Aluno;
@@ -16,37 +17,66 @@ import br.com.vemprafam.pojo.Aluno;
 @RequestMapping("/alunos")
 public class AlunosController {
 
-	private DaoAluno dao = new DaoAluno();
+    private DaoAluno dao = new DaoAluno();
 
-	@GetMapping
+    @GetMapping
     public String homeEmpty() {
         return "alunos"; // Retorna o nome do template da página inicial
     }
 
-	@GetMapping("/")
+    @GetMapping("/")
     public String home() {
         return "alunos"; // Retorna o nome do template da página inicial
     }
 
-	@GetMapping("/new")
-	public String showInsertForm(Model model) {
-		model.addAttribute("aluno", new Aluno());
-		return "create-aluno";
-	}
+    @GetMapping("/new")
+    public String showInsertForm(Model model) {
+        model.addAttribute("aluno", new Aluno());
+        return "create-aluno";
+    }
 
-	@PostMapping
-	public String createAluno(@ModelAttribute Aluno aluno) {
-		dao.insert(aluno);
-		return "redirect:alunos";
-	}
+    @PostMapping
+    public String createAluno(@ModelAttribute Aluno aluno) {
+        dao.insert(aluno);
+        return "redirect:alunos";
+    }
 
-	@GetMapping("/lista")
-	public String showLista(Model model){
-		List<Aluno> lista = dao.getLista();
-		model.addAttribute("alunos", lista);
-		return "lista-alunos";
-	}
+    @GetMapping("/lista")
+    public String showLista(Model model){
+        List<Aluno> lista = dao.getLista();
+        model.addAttribute("alunos", lista);
+        return "lista-alunos";
+    }
 
+    @GetMapping("/busca")
+    public String showBusca() {
+        return "busca-aluno";
+    }
+
+    @GetMapping("/showUpdate")
+    public String showEditAluno(@RequestParam int ra, Model model) {
+        Aluno aluno = dao.buscarPeloRa(ra);
+        model.addAttribute("aluno", aluno);
+        return "alterar-aluno";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute Aluno aluno) {
+        dao.update(aluno);
+        return "alunos";
+    }
+
+    @GetMapping("/apagar")
+    public String showDelete(Model model) {
+        Aluno aluno = new Aluno();
+        model.addAttribute("aluno", aluno);
+        return "apagar-aluno";
+    }
+    @PostMapping("/delete")
+    public String delete(@ModelAttribute Aluno aluno) {
+        dao.delete(aluno);
+        return "alunos";
+    }
 }
 
 
